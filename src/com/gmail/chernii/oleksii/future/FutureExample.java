864928dc83a1d20d34346fce72f51593002d35cd
AlexForJava <hqq@ukr.net>
2019-03-22 01:21:51 +0200

    third commit

--- src/com/gmail/chernii/oleksii/future/FutureExample.java
+++ src/com/gmail/chernii/oleksii/future/FutureExample.java
@@ -1,6 +1,10 @@
 package com.gmail.chernii.oleksii.future;
 
-import java.util.concurrent.*;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  * Created by Space on 19.03.2019.
